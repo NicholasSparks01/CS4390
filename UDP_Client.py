110012347739e287646b3b6f971bc4f0f8049713
Caleb McLaughlin <calebzm@bgsu.edu>
2023-10-24 15:03:44 -0400

    Implement Draft for UDP Client And Server

--- UDP_Client.py
+++ UDP_Client.py
@@ -1,1 +1,65 @@
-# Empty
+from socket import *
+
+def initialize_client():
+    server_name = 'localhost'
+    server_port = 11235
+    client_socket = socket(AF_INET, SOCK_DGRAM)
+
+    # Long message for testing
+    long_msg = """
+    If debugging is the process of removing software bugs,
+    then programming must be the process of putting them in.
+    ― Edsger W. Dijkstra
+    Your obligation is that of active participation.
+    You should not act as knowledge-absorbing sponges,
+    but as whetstones on which we can all sharpen our wits
+    ― Edsger W. Dijkstra
+    Sometimes it pays to stay in bed on Monday,
+    rather than spending the rest of the week debugging Monday’s code.
+    ― Dan Salomon
+    If carpenters made buildings the way programmers make programs,
+    the first woodpecker to come along would destroy all of civilization.
+    ― Unknown
+    """
+
+    sequence_number = 0  # Initial sequence number
+
+    # Send data using RDT
+    rdt_send(client_socket, long_msg, server_name, server_port, sequence_number)
+
+def rdt_send(client_socket, data, server_name, server_port, sequence_number):
+    end_of_message = False
+
+    while not end_of_message:
+        packet = f"{sequence_number}:{data[:200]}"  # Include sequence number and a chunk of data
+        data = data[200:]  # Remove the sent chunk
+
+        if not data:
+            end_of_message = True
+
+        # Send the packet to the server
+        client_socket.sendto(packet.encode(), (server_name, server_port))
+
+        attempts = 0
+        client_socket.settimeout(5)  # Set the timeout outside the loop
+
+        while attempts < 5:
+            try:
+                # Wait for an acknowledgment (ACK) from the server
+                response, server_address = client_socket.recvfrom(1536)
+                ack, _ = response.decode().split(':')
+                ack = int(ack)
+
+                if ack == sequence_number:
+                    print(f"Received ACK for sequence number {ack}")
+                    sequence_number += 1  # Increment sequence number
+                    break  # Exit the loop upon successful ACK
+            except timeout:
+                # Handle timeout (e.g., retransmit the packet)
+                attempts += 1
+
+        if attempts >= 5:
+            print("Connection failed to transmit packet")
+
+if __name__ == "__main__":
+    initialize_client()
--- UDP_Server.py
+++ UDP_Server.py
@@ -1,1 +1,36 @@
-# Empty
+from socket import *
+import sys
+
+def initialize_server():
+    server_port = 11235
+    server_socket = socket(AF_INET, SOCK_DGRAM)
+    server_socket.bind(('', server_port))
+
+    print("The server is ready to receive")
+
+    expected_sequence_number = 0
+    received_data = ""
+
+    # Receive data using RDT
+    while True:
+        data, client_address = server_socket.recvfrom(1536)
+        received_sequence_number, received_chunk = data.decode().split(':')
+
+        if int(received_sequence_number) == expected_sequence_number:
+            print(f"Received data with sequence number {received_sequence_number}: {received_chunk}")
+            expected_sequence_number += 1  # Increment sequence number
+            received_data += received_chunk
+
+            # Simulate sending an ACK
+            ack_packet = f"{received_sequence_number}:ACK"
+            server_socket.sendto(ack_packet.encode(), client_address)
+
+            if not received_chunk:
+                break  # End of message
+
+def shutdown_server(sig, frame):
+    print("Shutting down the server...")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    initialize_server()
