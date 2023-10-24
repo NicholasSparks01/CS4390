9d25aa1ba12de1fbe58a2f391f700945e8027e91
NicholasSparks01 <93989654+NicholasSparks01@users.noreply.github.com>
2023-10-24 10:35:21 -0400

    IP address function

--- ProxyServer.py
+++ ProxyServer.py
@@ -2,6 +2,28 @@ from socket import *
 import sys
 import signal
 
+def get_local_ip_address():
+    try:
+        # Create a socket object to get the local IP address
+        sock = socket(AF_INET, SOCK_DGRAM)
+        sock.settimeout(0.1)
+
+        # Connect to a public IP address (doesn't send any data)
+        sock.connect(("8.8.8.8", 80))
+
+        # Get the local IP address from the connected socket
+        local_ip_address = sock.getsockname()[0]
+
+        # Close the socket
+        sock.close()
+
+        return local_ip_address
+    except socket.error:
+        print("Could not retrieve local IP address")
+        return -1
+local_ip = get_local_ip_address()
+print("Local IP Address:", local_ip)
+
 def initialize_server(config):
     # Create server socket, bind to port, and listen
     tcpSerSock = socket(AF_INET, SOCK_STREAM)
@@ -30,7 +52,7 @@ if len(sys.argv) > 1:
 
 # Initialize the server
 config = {
-    'Proxy_Client': '192.168.68.68',    # Replace with your actual IP if needed
+    'Proxy_Client': local_ip,    # <<<< REPLACE WITH ACTUAL IP ADDRESS if script doesn't work<<<<<
     'MAX_REQUEST_LEN' : 4096,       # Maximum request length
     'CONNECTION_TIMEOUT' : 30        # Number of attempts to connect
     }  
@@ -49,8 +71,10 @@ while True:
 
         if not request:
             continue
-
-        print("Received Request:", request.decode())
+        
+        print("+-+-+-+-+-Received Request RAW+-+-+-+-+-")
+        print(request.decode())
+        print("+-+-+-+-+-End of Request RAW+-+-+-+-+-")
 
         # Get Domain name
         first_line = request.split(b'\n')[0]
@@ -59,18 +83,22 @@ while True:
         # Parse the URL and extract hostname  
         if (i == 0):
             hostname = url.decode()[1:]
+        print("===================")
         print("Hostname:", hostname)        
-        
-        # Parsing
+        print("===================")
+
+        # if first request, ask for domain request, else ask for files associated with first request
         if (i == 0):
             new_request = request[:4].decode() + "http://" + request[5:].decode()
         else:
             new_request = request[:4].decode() + "http://" + hostname + "/" + request[5:].decode()
 
-        print("NEW:", new_request)
-        new_request = new_request.encode()
+        print("+-+-+-+-+-NEW Formatted Request+-+-+-+-+-")
+        print(new_request)
+        print("+-+-+-+-+-End of NEW Request+-+-+-+-+-")
 
-            
+        new_request = new_request.encode()
+ 
         # Handle the request to destination server
         s = socket(AF_INET, SOCK_STREAM)
         s.settimeout(config['CONNECTION_TIMEOUT'])
@@ -78,7 +106,7 @@ while True:
 
         try:
             s.connect((hostname, 80))
-            print("SENDING REQUEST")
+            print("SENDING REQUEST TO DOMAIN....")
             s.sendall(new_request)
 
             # Receive the response from the destination server
@@ -87,7 +115,9 @@ while True:
                 data = s.recv(4096)
                 print("")
                 print("")
+                print("+-+-+-+-+-RESPONSE FROM DOMAIN+-+-+-+-+-")
                 print(data)
+                print("+-+-+-+-+-END OF DOMAIN RESPONSE+-+-+-+-+-")
                 # Continue until data is empty
                 if (len(data) > 0):
                     tcpCliSock.send(data)
