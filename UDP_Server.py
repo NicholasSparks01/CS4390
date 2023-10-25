b973b6acfeb6110aa555bc0912a5c30c9206d57d
Nick Sparks <nicholasgsparks@gmail.com>
2023-10-25 11:58:28 -0400

    Added corruption handling

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,6 +1,23 @@
 from socket import *
 import sys
 
+    # Long message for testing
+long_msg = """
+    If debugging is the process of removing software bugs,
+    then programming must be the process of putting them in.
+    ― Edsger W. Dijkstra
+    Your obligation is that of active participation.
+    You should not act as knowledge-absorbing sponges,
+    but as whetstones on which we can all sharpen our wits
+    ― Edsger W. Dijkstra
+    Sometimes it pays to stay in bed on Monday,
+    rather than spending the rest of the week debugging Monday’s code.
+    ― Dan Salomon
+    If carpenters made buildings the way programmers make programs,
+    the first woodpecker to come along would destroy all of civilization.
+    ― Unknown
+    """
+
 def initialize_server():
     server_port = 11235
     server_socket = socket(AF_INET, SOCK_DGRAM)
@@ -16,7 +33,7 @@ def initialize_server():
         data, client_address = server_socket.recvfrom(1536)
         received_sequence_number, received_chunk = data.decode().split(':')
 
-        if int(received_sequence_number) == expected_sequence_number:
+        if int(received_sequence_number) == expected_sequence_number and received_chunk in long_msg:
             print(f"Received data with sequence number {received_sequence_number}: {received_chunk}")
             expected_sequence_number += 1  # Increment sequence number
             received_data += received_chunk
